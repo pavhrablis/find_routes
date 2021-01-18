b011531dd82c0c1e2c793fcb456559ed004e5ea5
pavhrablis <palik1hrablis@gmail.com>
2021-01-18 17:20:38 +0100

    add cities view

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from cities.models import City
+
+__all__ = (
+    'home',
+)
+
+
+def home(request, pk=None):
+    if pk:
+        # city = City.objects.filter(id=pk).first()
+        city = get_object_or_404(City, id=pk)
+        context = {'object': city}
+        return render(request, 'cities/detail.html', context)
+    cities = City.objects.all()
+    context = {'objects_list': cities}
+    return render(request, 'cities/home.html', context)
